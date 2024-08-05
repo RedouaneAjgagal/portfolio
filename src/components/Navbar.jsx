8e84f67c906c993c81499c13a53ec76696121751
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-08-05 22:50:30 +0100

    [FEAT] responsive home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,10 +46,10 @@ const Navbar = () => {
   }
 
   return (
-    <nav className={`w-full flex flex-col px-4 ${isNavMenuOpen ? "" : "sm:flex-row sm:items-center sm:gap-8 md:gap-16"}`}>
-      <div className="min-w-fit flex items-center justify-between gap-4 h-16">
+    <nav className={`p-4 w-full h-full flex flex-col ${isNavMenuOpen ? "" : "lg:grid lg:grid-cols-5 lg:p-0"}`}>
+      <div className={`min-w-fit flex items-center justify-between gap-4 ${isNavMenuOpen ? "" : "md:gap-10 lg:gap-12 xl:gap-16 lg:col-span-3 lg:pl-10 xl:pl-16 2xl:gap-20"}`}>
         <div className="relative z-10">
-          <Link to={"/"} className={`font-medium text-xl ${isNavMenuOpen ? "text-slate-50" : "text-black"} sm:text-2xl`}>Redouane Ajgagal</Link>
+          <Link to={"/"} className={`font-montserrat uppercase font-medium text-3xl ${isNavMenuOpen ? "text-slate-50" : "text-black"} sm:text-4xl`}>Ajgagal</Link>
         </div>
         <div className={`mt-1 transition-all flex ${isNavMenuOpen ? "" : "sm:hidden"}`}>
           <button onClick={toggleNavMenuHandler} className={`relative z-10 w-8 h-7 flex items-center before:absolute before:left-1 before:top-1 before:w-6 before:h-1 before:rounded-full after:absolute after:bottom-1 after:left-1 after:w-6 after:h-1 after:rounded-full before:transition-all after:transition-all 
@@ -60,14 +60,20 @@ const Navbar = () => {
             <span className={`w-5 ml-1 h-1 rounded-full bg-slate-700  ${isNavMenuOpen ? "invisible" : "visible"}`}></span>
           </button>
         </div>
+        <div className={`fixed left-0 min-h-svh bg-blue-500 w-full grid content-center ${isNavMenuOpen ? "top-0" : "-top-full sm:top-0 sm:static sm:min-h-min sm:bg-transparent"} transition-all`}>
+          <ul className={`font-serif font-medium uppercase flex flex-col items-center w-full relative gap-8 text-lg md:text-xl ${isNavMenuOpen ? "" : "sm:flex-row sm:relative sm:gap-6 md:gap-8 xl:gap-12 2xl:gap-16"}`}>
+            <li><NavLink onClick={closeNavbarMenuHandler} to="/" className={({ isActive }) => `${isActive ? `underline decoration-2 ${isNavMenuOpen ? "text-slate-50 decoration-orange-500 underline-offset-8" : "sm:text-sky-600 sm:decoration-sky-600 sm:underline-offset-4"}` : isNavMenuOpen ? "text-white" : "sm:no-underline "}`}>Home</NavLink></li>
+            <li className="min-w-fit"><NavLink onClick={closeNavbarMenuHandler} to="/about" className={({ isActive }) => `${isActive ? `underline decoration-2 ${isNavMenuOpen ? "text-slate-50 decoration-orange-500 underline-offset-8" : "sm:text-sky-600 sm:decoration-sky-600 sm:underline-offset-4"}` : isNavMenuOpen ? "text-white" : "sm:no-underline"}`}>About Me</NavLink></li>
+            <li><NavLink onClick={closeNavbarMenuHandler} to="/projects" className={({ isActive }) => `${isActive ? `underline decoration-2 ${isNavMenuOpen ? "text-slate-50 decoration-orange-500 underline-offset-8" : "sm:text-sky-600 sm:decoration-sky-600 sm:underline-offset-4"}` : isNavMenuOpen ? "text-white" : "sm:no-underline"}`}>Projects</NavLink></li>
+            <li className={`${isNavMenuOpen ? "" : "lg:hidden"}`}><NavLink onClick={closeNavbarMenuHandler} to="/contact" className={({ isActive }) => `${isActive ? `underline decoration-2 ${isNavMenuOpen ? "text-slate-50 decoration-orange-500 underline-offset-8" : "sm:text-sky-600 sm:decoration-sky-600 sm:underline-offset-4 sm:absolute sm:right-0 sm:top-0"}` : isNavMenuOpen ? "text-white" : "sm:no-underline sm:absolute sm:right-0 sm:top-0 lg:text-white lg:border-2 lg:px-4 lg:py-1 lg:border-white lg:rounded-tl-2xl lg:rounded-br-2xl lg:-top-1"}`}>Contact</NavLink></li>
+          </ul>
+        </div>
       </div>
-      <div className={`fixed left-0 min-h-svh bg-blue-500 w-full grid content-center ${isNavMenuOpen ? "top-0" : "-top-full sm:top-0 sm:static sm:min-h-min sm:bg-transparent"} transition-all`}>
-        <ul className={`font-medium uppercase flex flex-col items-center w-full relative gap-8 text-lg ${isNavMenuOpen ? "" : "sm:flex-row sm:relative sm:mt-1 sm:gap-6 md:gap-12"}`}>
-          <li><NavLink onClick={closeNavbarMenuHandler} to="/" className={({ isActive }) => `${isActive ? `underline decoration-2 ${isNavMenuOpen ? "text-slate-50 decoration-orange-500 underline-offset-8" : "sm:text-sky-600 sm:decoration-sky-600 sm:underline-offset-4"}` : isNavMenuOpen ? "text-white" : "sm:no-underline"}`}>Home</NavLink></li>
-          <li><NavLink onClick={closeNavbarMenuHandler} to="/about" className={({ isActive }) => `${isActive ? `underline decoration-2 ${isNavMenuOpen ? "text-slate-50 decoration-orange-500 underline-offset-8" : "sm:text-sky-600 sm:decoration-sky-600 sm:underline-offset-4"}` : isNavMenuOpen ? "text-white" : "sm:no-underline"}`}>About Me</NavLink></li>
-          <li><NavLink onClick={closeNavbarMenuHandler} to="/projects" className={({ isActive }) => `${isActive ? `underline decoration-2 ${isNavMenuOpen ? "text-slate-50 decoration-orange-500 underline-offset-8" : "sm:text-sky-600 sm:decoration-sky-600 sm:underline-offset-4"}` : isNavMenuOpen ? "text-white" : "sm:no-underline"}`}>Projects</NavLink></li>
-          {/* <li><NavLink to="/contact" className={({ isActive }) => `text-slate-50 ${isActive ? "underline underline-offset-8 decoration-2 decoration-orange-500" : isNavMenuOpen ? "" : "sm:text-slate-800 sm:absolute sm:right-0 sm:top-0"}`}>Contact</NavLink></li> */}
-          <li><NavLink onClick={closeNavbarMenuHandler} to="/contact" className={({ isActive }) => `${isActive ? `underline decoration-2 ${isNavMenuOpen ? "text-slate-50 decoration-orange-500 underline-offset-8" : "sm:text-sky-600 sm:decoration-sky-600 sm:underline-offset-4 sm:absolute sm:right-0 sm:top-0"}` : isNavMenuOpen ? "text-white" : "sm:no-underline sm:text-slate-800 sm:absolute sm:right-0 sm:top-0"}`}>Contact</NavLink></li>
+      <div className={`hidden ${isNavMenuOpen ? "" : "lg:col-span-2 lg:flex lg:items-center lg:justify-end lg:bg-blue-500 lg:pr-10 xl:pr-16"}`}>
+        <ul className="flex gap-4 font-serif font-medium uppercase text-lg lg:text-xl">
+          <li>
+            <NavLink onClick={closeNavbarMenuHandler} to="/contact" className="text-slate-50 font- border-2 px-4 py-2 border-white rounded-tl-2xl rounded-br-2xl">Contact</NavLink>
+          </li>
         </ul>
       </div>
     </nav>
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -3,11 +3,11 @@ import { Navbar } from "../components"
 
 const Root = () => {
     return (
-        <div className="bg-slate-50">
-            <header>
+        <div className="bg-slate-50 lg:min-h-dvh lg:h-full lg:flex lg:flex-col">
+            <header className="h-16 w-full lg:h-20">
                 <Navbar />
             </header>
-            <main className="min-h-svh">
+            <main className="w-full mx-auto">
                 <Outlet />
             </main>
         </div>
