1bded5819d7915dc909fedd4f80f69645b78cc7b
RedouaneAjgagal <redouaneajgagal1@gmail.com>
2024-08-15 20:15:43 +0100

    [IMPROVE] better scroll bar - express icon - form button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -296,10 +296,16 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className="col-span-1 lg:col-span-2">
-                    <button disabled={contactRequest.isLoading} className="bg-neutral-800/10 p-3 rounded-sm border-2 border-neutral-300/20 w-full font-medium text-slate-300 uppercase transition-all hover:bg-neutral-900">
+                    <button disabled={contactRequest.isLoading} className="bg-zinc-900 p-3 rounded-sm border-2 border-neutral-300/20 w-full font-medium text-slate-300 uppercase transition-all hover:bg-neutral-900">
                         {contactRequest.isLoading
-                            ? "Sending.."
-                            : "Send Message"
+                            ? <span>Sending..</span>
+                            : <span className="flex items-center gap-2 justify-center">Send Message
+                                <div className="w-4 h-full">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="#cbd5e1">
+                                        <path d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376l0 103.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z" />
+                                    </svg>
+                                </div>
+                            </span>
                         }
                     </button>
                     <div className={`w-fit m-auto mt-2 text-center duration-200 px-8 rounded-sm font-medium ${!contactRequest.isVisible
